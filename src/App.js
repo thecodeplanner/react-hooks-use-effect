46e2dfca8db33b4554299cc0b18f874087a0eec6
thecodeplanner <thecodeplanner@gmail.com>
2021-01-13 19:49:22 -0500

    commit tree 0fba8dd1bb8ba4ca8590bdd94cf616730550bd9b

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    const timerID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    // returning a cleanup function
+    return function () {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  return <div>{time.toString()}</div>;
+}
+
+function App() {
+
+
+  const [showClock, setShowClock] = useState(true);
+
+
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    document.title = text;
+  }, [text])
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <>
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+    
+    <div>
+          {showClock ? <Clock /> : null}
+          <button onClick={() => setShowClock(false)}>Hide Clock</button>
+      </div>
+      </>
+  )
 }
 
 export default App;
